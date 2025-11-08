b236e3a6406762ce3de38f22c79ecf5a60fc9784
yalyzak <132987109+yalyzak@users.noreply.github.com>
2025-11-08 20:51:27 +0200

    Add files via upload

--- bereshit/addons/essentials/Shoot.py
+++ bereshit/addons/essentials/Shoot.py
@@ -1,5 +1,7 @@
 from bereshit import Object,Camera,Vector3, Rigidbody, BoxCollider, MeshRander, Quaternion, Render, Physics
 import mouse  # pip install mouse
+from bereshit.render import Text
+
 import copy
 class Shoot:
     def __init__(self,target):
@@ -8,17 +10,28 @@ class Shoot:
         self.speed = 10
         self.target = target
         self.force = 10
+        self.shots = 10
+        self.shots_text = Text(str(self.shots), center=(120,850), scale=1)
 
+        self.gimos = Object(position=Vector3(1000,1000,1000),size=Vector3(.1,.1,.1))
 
     def onClick(self):
         # print("g")
+        if self.shots == 0:
+            self.shots = 10
+        self.shots -= 1
+        self.shots_text.text = str(self.shots)
+        # self.render.t = str(self.shots)
         forward = self.parent.quaternion.rotate(Vector3(0,0,1))
         hit = Physics.Raycast(self.parent.position.to_np(),forward.to_np(),self.target.get_component("collider"))
-        if hit is not None:
-            self.target.Rigidbody.AddForce(forward * self.force,ContactPoint=Vector3.from_np(hit))
-            print(forward * self.force)
+        if hit.point is not None:
+            self.target.Rigidbody.AddForce(forward * self.force,ContactPoint=Vector3.from_np(hit.point))
+            # self.gimos.position = Vector3.from_np(hit)
     def Start(self):
         self.render = self.parent.Camera.render
+        self.target.add_child(self.gimos)
+        self.render.add_text_rect(self.shots_text)
+
     def Update(self, dt):
 
         # advance the timer
