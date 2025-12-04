f8f5bb91eb7a574990f011a498b30fd72ad25f00
yalyzak <132987109+yalyzak@users.noreply.github.com>
2025-12-04 21:28:03 +0200

    Add files via upload

---
+++ scene/Clientuse.py
@@ -0,0 +1,89 @@
+import socket
+import struct
+from collections import deque
+from bereshit import World, Object, Rigidbody, BoxCollider, Vector3, Quaternion, MeshRander
+
+
+FORMAT = "fff ffff fff"      # pos(3), quat(4), vel(3)
+SIZE = struct.calcsize(FORMAT)
+
+players = {}  # { "username": Object() }
+
+class Clientuse:
+    def __init__(self, data_objects=[]):
+        self.data_objects = data_objects
+        self.UserName = None
+
+        self.incoming = deque()  # stores raw bytes
+        self.outgoing = deque()  # stores raw bytes
+    def Start(self):
+        self.Broadcast = self.parent.Client.Broadcast
+        self.ReceiveMassages = self.parent.Client.ReceiveMassages
+
+        self.player = Object()
+        self.player.add_component(BoxCollider())
+        self.player.add_component(Rigidbody(useGravity=True))
+        ServerContiner = Object()
+        ServerContiner.add_component(MeshRander(shape="empty"))
+        self.parent.add_child(ServerContiner)
+        self.Continer = ServerContiner
+
+    def Send(self):
+        for obj in self.data_objects:
+            msg = struct.pack(
+                "fff fff ffff",
+                obj.position.x, obj.position.y, obj.position.z,
+                obj.quaternion.x, obj.quaternion.y, obj.quaternion.z, obj.quaternion.w,
+
+                obj.Rigidbody.velocity.x, obj.Rigidbody.velocity.y, obj.Rigidbody.velocity.z,
+
+            )
+            self.Broadcast(self.UserName,msg)
+
+    def Update(self, dt):
+        if self.UserName is not None:
+            self.Send()
+
+        msg = self.ReceiveMassages()
+        if not msg:
+            return
+
+        username = msg["from"]
+        raw = msg["message"]
+
+        # --- Ensure correct data size ---
+        if len(raw) != SIZE:
+            print("Bad message size from", username, len(raw))
+            return
+
+        # --- Unpack floats ---
+        px, py, pz, qx, qy, qz, qw, vx, vy, vz = struct.unpack(FORMAT, raw)
+
+        # --- Create new player if not in dictionary ---
+        if username not in players:
+            new_player = Object()
+            new_player.add_component(BoxCollider())
+            new_player.add_component(Rigidbody(useGravity=False))  # server controls vel
+
+            # Optional: add a renderer so you can see the player
+            new_player.add_component(MeshRander(shape="cube"))
+
+            # Add to world
+            self.Continer.add_child(new_player)
+
+            players[username] = new_player
+            print("Created new player:", username)
+
+        # --- Update the player ---
+        player = players[username]
+
+        player.position.x = px
+        player.position.y = py
+        player.position.z = pz
+
+        player.quaternion = Quaternion(qx, qy, qz, qw)
+
+        rb = player.get_component(Rigidbody)
+        rb.velocity = Vector3(vx, vy, vz)
+
+
